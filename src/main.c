a8e73eb097fc11b79f6b89cba096347b03d1c43a
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2022-02-15 10:46:39 +0100

    added support for detail selection

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@
 int main(void) {
 	int width, height;
 	unsigned char *data = stbi_load("train.png", &width, &height, NULL, 3);
-	char *str = convert_image(data, width, height, 200, 60);
+	char *str = convert_image(data, width, height, 800 / 8 * 1.5, 640 / 16 * 1.5, 6);
 	printf("%s\n", str);
+
+	// for (int i = 0; i < 7; i++) print_allowed_characters(i);
 }
